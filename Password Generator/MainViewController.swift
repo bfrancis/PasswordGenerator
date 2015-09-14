10d24bb10b906206c96899916279671f76dafb41
bfrancis <bfrancis@friends.tas.edu.au>
2015-09-14 11:34:40 +1000

    'Select All', 'Unselect All' and 'Copy to Clipboard' buttons now work as expected. Ready to export project and have it tested by others.

--- Password Generator/MainViewController.swift
+++ Password Generator/MainViewController.swift
@@ -14,6 +14,7 @@ class MainViewController: NSViewController {
     var numPasswordsCounter = 1
     var numPasswordsFormatter = IntegerOnlyFormatter()
     var passwords = [Password]()
+    var pasteboard = NSPasteboard.generalPasteboard()
     let generator = PasswordGenerator()
     
     @IBOutlet weak var numPasswordsTextField: NSTextField!
@@ -49,6 +50,53 @@ class MainViewController: NSViewController {
         }
     }
     
+    func getAllCheckboxes() -> [NSButton] {
+        var checkboxes = [NSButton]()
+        
+        // The index of the column containing the checkboxes
+        var checkboxColumnIndex = passwordsTableView.columnWithIdentifier("SelectBoxColumn")
+        
+        // The number of rows in the Table View
+        var rowsCount = self.numberOfRowsInTableView(passwordsTableView)
+        
+        // Iterate through each row of the Table View
+        for row in (0..<rowsCount) {
+            // The view containing the check box
+            var view = passwordsTableView.viewAtColumn(checkboxColumnIndex, row: row, makeIfNecessary: false) as! NSView
+            
+            // An array of subviews within the checkbox column
+            var subviews = view.subviews
+            
+            // Iterate through all the subviews and try to find
+            // an NSButton object (the checkbox)
+            for i in (0..<subviews.count) {
+                var currentSubView: AnyObject = subviews[i]
+                
+                // Attempt to downcast the current subview (AnyObject) to an NSButton
+                if let checkbox = currentSubView as? NSButton {
+                    checkboxes.append(checkbox)
+                }
+            }
+        }
+        
+        return checkboxes
+    }
+    
+    func getPasswordAtRow(row: Int) -> String {
+        var passwordColumnIndex = passwordsTableView.columnWithIdentifier("PasswordColumn")
+        
+        // The view containing the password
+        var view = passwordsTableView.viewAtColumn(passwordColumnIndex, row: row, makeIfNecessary: false) as! NSView
+        
+        if let cellView = view as? NSTableCellView {
+            if let textField = cellView.textField {
+                return textField.stringValue
+            }
+        }
+        
+        return String()
+    }
+
     func updateCounterControls() {
         numPasswordsStepper.integerValue = numPasswordsCounter
         numPasswordsTextField.integerValue = numPasswordsCounter
@@ -80,6 +128,11 @@ extension MainViewController {
         
         debug("Number of rows in table: \(range.length)")
         
+        // Pretend 'Unselect All' button was pressed. 
+        // (A lazy way to unselect all the checkboxes before they get recreated)
+        self.unselectAllButtonPressed(sender)
+        
+        // Remove all the passwords
         self.passwordsTableView.removeRowsAtIndexes(NSIndexSet(indexesInRange: range), withAnimation: nil)
         
         // Empty passwords array
@@ -91,30 +144,58 @@ extension MainViewController {
             passwords.append(pass)
         }
         
+        // Update the length on the range (NSRange)
         range.length = passwords.endIndex
         
         self.passwordsTableView.insertRowsAtIndexes(NSIndexSet(indexesInRange: range), withAnimation: NSTableViewAnimationOptions.EffectGap)
     }
     
     @IBAction func selectAllButtonPressed(sender: NSButton) {
-        var checkboxColumnIndex = passwordsTableView.columnWithIdentifier("SelectBoxColumn")
-        var rowsCount = self.numberOfRowsInTableView(passwordsTableView)
         
-        debug("Number of rows: \(rowsCount)")
+        // Get all the checkboxes in the Table View
+        var checkboxes = getAllCheckboxes()
         
-        for row in (0..<rowsCount) {
-            var view = passwordsTableView.viewAtColumn(checkboxColumnIndex, row: row, makeIfNecessary: false) as! NSView
-            
-            var subviews = view.subviews
-            
-            for i in (0..<subviews.count) {
-                var currentSubView: AnyObject = subviews[i]
+        // Iterate through and set them all to "On" (selected)
+        for box in checkboxes {
+            box.state = NSOnState
+        }
+    }
+    
+    @IBAction func unselectAllButtonPressed(sender: NSButton) {
+        
+        // Get all the checkboxes in the Table View
+        var checkboxes = getAllCheckboxes()
+        
+        // Iterate through and set them all to "Off" (unselected)
+        for box in checkboxes {
+            box.state = NSOffState
+        }
+    }
+    
+    @IBAction func copySelectedButtonPressed(sender: NSButton) {
+        var checkboxes = getAllCheckboxes()
+        var copyToClipboard = String()
+        var password = String()
+        
+        // Clear the contents of the clipboard
+        pasteboard.clearContents()
+        
+        for i in (0..<checkboxes.count) {
+            if (checkboxes[i].state == NSOnState) {
+               password = getPasswordAtRow(i)
                 
-                if let checkbox = currentSubView as? NSButton {
-                    checkbox.
-                }
+                // Add the password to the string that gets copied
+                // to the clipboard
+                copyToClipboard += password
+                copyToClipboard += "\n"
             }
         }
+        
+        if (count(copyToClipboard) > 0) {
+            debug("Copying \(self.passwords.count) passwords to clipboard")
+            pasteboard.writeObjects([copyToClipboard])
+        }
+        
     }
 }
 
@@ -134,11 +215,6 @@ extension MainViewController: NSTableViewDataSource {
             cellView.textField!.stringValue = password.value
             return cellView
         }
-        
-        // Create a select box cell view
-//        if tableColumn!.identifier == "SelectBoxColumn" {
-//            var selectBox = NSCheck
-//        }
         
         return cellView
     }
